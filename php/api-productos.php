066bc066727eaf7454733e15a02072879c1db79d
Geovanni-Vera <escolar.geovanni@gmail.com>
2023-10-23 01:00:40 -0600

    Se agregaron grandes cambios a la apariencia y un crud

--- php/api-productos.php
+++ php/api-productos.php
@@ -1,30 +1,23 @@
 <?php
 
 // Conexión a la base de datos
-$host = "localhost";
-$dbname = "productosapi"; // Cambia a tu nombre de BD
-$user = "root"; // Cambia a tu usuario
-$pass = "root"; // Cambia a tu contraseña
+require "config/database.php";
+$db = new Database("localhost","productosapi","root","root");
+$connect = $db -> connect();
+if($connect){
+    // Consulta a la tabla de productos
+    $query = "SELECT * FROM productos";
 
-try {
-    $conexion = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
-    exit;
+    $result = $db->query($query);
+    // Arreglo para almacenar los productos
+    $productos = array();
+
+    // Almacenar los productos en el arreglo
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $productos[] = $row;
+    }
+
+    // Enviar los productos como JSON
+    header("Content-Type: application/json");
+    echo json_encode($productos);
 }
-
-// Consulta a la tabla de productos
-$query = $conexion->query("SELECT * FROM productos");
-
-// Arreglo para almacenar los productos
-$productos = array();
-
-// Almacenar los productos en el arreglo
-while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-    $productos[] = $row;
-}
-
-// Enviar los productos como JSON
-header("Content-Type: application/json");
-echo json_encode($productos);
-?>
